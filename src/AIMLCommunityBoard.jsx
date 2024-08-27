d8045c1289480ba035e0d70777680a9992de9961
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-08-27 00:49:38 -0400

    chore: Refactor AIMLCommunityBoard component to improve event filtering and add search by date functionality

--- src/AIMLCommunityBoard.jsx
+++ src/AIMLCommunityBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import { Calendar, Clock, MapPin, Link, Search } from "lucide-react";
 import { events } from "./eventData";
